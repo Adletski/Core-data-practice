d6d3b04390aeff28b4b399cafb6f2d77e01d2575
Adlet <adlet.zh19966@gmail.com>
2023-03-30 15:18:07 +0600

    Initial Commit

---
+++ core data practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  core data practice
+//
+//  Created by Adlet Zhantassov on 30.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
